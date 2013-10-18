aa992ac2e31a454d2cda266449747966b443242c
fyraimar <fyraimar@gmail.com>
2013-10-18 09:47:30 +0800

    modify disp api

--- runtime/android/java/src/org/xwalk/runtime/extension/api/DeviceCapabilitiesDisplay.java
+++ runtime/android/java/src/org/xwalk/runtime/extension/api/DeviceCapabilitiesDisplay.java
@@ -14,6 +14,10 @@ import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.Display;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,58 +34,91 @@ public class DeviceCapabilitiesDisplay {
     private boolean mIsRegisteredOnConnect = false;
     private boolean mIsRegisteredOnDisconnect = false;
 
-    private boolean mIsInternal;
-    private boolean mIsPrimary;
-    private int mDpiX;
-    private int mDpiY;
-    private long mAvailableHeight;
-    private long mAvailableWidth;
-    private long mHeight;
-    private long mWidth;
-    private String mDisplayId;
-    private String mDisplayName;
+    class DisplayInfo {
+        private boolean mIsInternal;
+        private boolean mIsPrimary;
+        private int mDpiX;
+        private int mDpiY;
+        private long mAvailableHeight;
+        private long mAvailableWidth;
+        private long mHeight;
+        private long mWidth;
+        private String mDisplayId;
+        private String mDisplayName;
 
+        private DisplayInfo(boolean isInternal,
+                            boolean isPrimary,
+                            int dpiX,
+                            int dpiY,
+                            long availableHeight,
+                            long availableWidth,
+                            long height,
+                            long width,
+                            String displayId,
+                            String displayName) {
+            mIsInternal = isInternal;
+            mIsPrimary = isPrimary;
+            mDpiX = dpiX;
+            mDpiY = dpiY;
+            mAvailableHeight = availableHeight;
+            mAvailableWidth = availableWidth;
+            mHeight = height;
+            mWidth = width;
+            mDisplayId = displayId;
+            mDisplayName = displayName;
+        }
+    }
+
+    private Map<String, DisplayInfo> mDisplays = new HashMap<String, DisplayInfo>();
     private final Handler mHandler = new Handler();
 
     private final DisplayListener mOnConnectListener = new DisplayListener() {
         @Override
         public void onDisplayAdded(int displayId) {
-            JSONObject displayObject = new JSONObject();
+            updateInfo();
+            JSONObject outputObject = new JSONObject();
+            JSONObject displayObject = getInfo(String.valueOf(displayId));
             try {
-                displayObject.put("reply", "connectDisplay");
-                displayObject.put("eventName", "onconnect");
-                displayObject.put("content", displayId);
+                outputObject.put("reply", "connectDisplay");
+                outputObject.put("eventName", "onconnect");
+                outputObject.put("content", displayObject);
             } catch (JSONException e) {
                 mDeviceCapabilities.sendErrorMessage(e);
             }
-            mDeviceCapabilities.postMessage(displayObject.toString());
-        }
-        @Override
-        public void onDisplayChanged(int arg0) {
-        }
-        @Override
-        public void onDisplayRemoved(int arg0) {
-        }
-    };
-
-    private final DisplayListener mOnDisconnectListener = new DisplayListener() {
-        @Override
-        public void onDisplayAdded(int displayId) {
+            mDeviceCapabilities.postMessage(outputObject.toString());
         }
         @Override
         public void onDisplayChanged(int arg0) {
         }
         @Override
         public void onDisplayRemoved(int displayId) {
-            JSONObject displayObject = new JSONObject();
+            if (!mIsRegisteredOnDisconnect)
+                updateInfo();
+        }
+    };
+
+    private final DisplayListener mOnDisconnectListener = new DisplayListener() {
+        @Override
+        public void onDisplayAdded(int displayId) {
+            if (!mIsRegisteredOnConnect)
+                updateInfo();
+        }
+        @Override
+        public void onDisplayChanged(int arg0) {
+        }
+        @Override
+        public void onDisplayRemoved(int displayId) {
+            JSONObject outputObject = new JSONObject();
+            JSONObject displayObject = getInfo(String.valueOf(displayId));
+            updateInfo();
             try {
-                displayObject.put("reply", "disconnectDisplay");
-                displayObject.put("eventName", "ondisconnect");
-                displayObject.put("content", displayId);
+                outputObject.put("reply", "disconnectDisplay");
+                outputObject.put("eventName", "ondisconnect");
+                outputObject.put("content", displayObject);
             } catch (JSONException e) {
                 mDeviceCapabilities.sendErrorMessage(e);
             }
-            mDeviceCapabilities.postMessage(displayObject.toString());
+            mDeviceCapabilities.postMessage(outputObject.toString());
         }
     };
 
@@ -97,27 +134,12 @@ public class DeviceCapabilitiesDisplay {
         JSONObject outputObject = new JSONObject();
         JSONArray outputArray = new JSONArray();
 
-        mContext = mExtensionContext.getContext();
-        mDisplayManager =
-                (DisplayManager) mContext.getSystemService(Context.DISPLAY_SERVICE);
-        Display[] dispArr = mDisplayManager.getDisplays();
-
+        updateInfo();
         try {
-            for (Display disp : dispArr) {
-                JSONObject displayObject = new JSONObject();
-                if (getDisplayInfo(disp)) {
-                    displayObject.put("id", mDisplayId);
-                    displayObject.put("name", mDisplayName);
-                    displayObject.put("isPrimary", mIsPrimary);
-                    displayObject.put("isInternal", mIsInternal);
-                    displayObject.put("dpiX", mDpiX);
-                    displayObject.put("dpiY", mDpiY);
-                    displayObject.put("width", mWidth);
-                    displayObject.put("height", mHeight);
-                    displayObject.put("availWidth", mAvailableWidth);
-                    displayObject.put("availHeight", mAvailableHeight);
-                    outputArray.put(displayObject);
-                }
+            Iterator<String> iter = mDisplays.keySet().iterator();
+            while (iter.hasNext()) {
+                JSONObject displayObject = getInfo(iter.next());
+                outputArray.put(displayObject);
             }
             outputObject.put("displays", outputArray);
         } catch (JSONException e) {
@@ -126,10 +148,78 @@ public class DeviceCapabilitiesDisplay {
         return outputObject;
     }
 
+    public JSONObject getInfo(String displayId) {
+        DisplayInfo displayInfo = mDisplays.get(displayId);
+        JSONObject retObject = new JSONObject();
+
+        try {
+            retObject.put("id", displayInfo.mDisplayId);
+            retObject.put("name", displayInfo.mDisplayName);
+            retObject.put("isPrimary", displayInfo.mIsPrimary);
+            retObject.put("isInternal", displayInfo.mIsInternal);
+            retObject.put("dpiX", displayInfo.mDpiX);
+            retObject.put("dpiY", displayInfo.mDpiY);
+            retObject.put("width", displayInfo.mWidth);
+            retObject.put("height", displayInfo.mHeight);
+            retObject.put("availWidth", displayInfo.mAvailableWidth);
+            retObject.put("availHeight", displayInfo.mAvailableHeight);
+        } catch (JSONException e) {
+            return setErrorMessage(e.toString());
+        }
+        return retObject;
+    }
+
+    private void updateInfo() {
+        mContext = mExtensionContext.getContext();
+        mDisplayManager =
+                (DisplayManager) mContext.getSystemService(Context.DISPLAY_SERVICE);
+        Display[] dispArr = mDisplayManager.getDisplays();
+        // is this nessary?
+        mDisplays.clear();
+
+        for (Display display : dispArr) {
+            String displayId = String.valueOf(display.getDisplayId());
+            String displayName = display.getName();
+            boolean isPrimary;
+            boolean isInternal;
+            if (displayId.equals(String.valueOf(display.DEFAULT_DISPLAY))) {
+                isPrimary = true;
+                isInternal = true;
+            } else {
+                isPrimary = false;
+                isInternal = false;
+            }
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            display.getRealMetrics(displayMetrics);
+            int dpiX = (int) displayMetrics.xdpi;
+            int dpiY = (int) displayMetrics.ydpi;
+            Point outSize = new Point();
+            display.getRealSize(outSize);
+            int width = outSize.x;
+            int height = outSize.y;
+            display.getSize(outSize);
+            int availableWidth = outSize.x;
+            int availableHeight = outSize.y;
+            DisplayInfo newDisplay = new DisplayInfo(isInternal,
+                                                     isPrimary,
+                                                     dpiX,
+                                                     dpiY,
+                                                     availableHeight,
+                                                     availableWidth,
+                                                     height,
+                                                     width,
+                                                     displayId,
+                                                     displayName);
+            mDisplays.put(displayId, newDisplay);
+        }
+    }
+
     public void registerOnConnectListener() {
         mContext = mExtensionContext.getContext();
         mDisplayManager =
                 (DisplayManager) mContext.getSystemService(Context.DISPLAY_SERVICE);
+        if (!mIsRegisteredOnDisconnect)
+            updateInfo();
         if (!mIsRegisteredOnConnect) {
             mDisplayManager.registerDisplayListener(mOnConnectListener, mHandler);
             mIsRegisteredOnConnect = true;
@@ -140,6 +230,8 @@ public class DeviceCapabilitiesDisplay {
         mContext = mExtensionContext.getContext();
         mDisplayManager =
                 (DisplayManager) mContext.getSystemService(Context.DISPLAY_SERVICE);
+        if (!mIsRegisteredOnConnect)
+            updateInfo();
         if (!mIsRegisteredOnDisconnect) {
             mDisplayManager.registerDisplayListener(mOnDisconnectListener, mHandler);
             mIsRegisteredOnDisconnect = true;
@@ -155,30 +247,6 @@ public class DeviceCapabilitiesDisplay {
         }
         mIsRegisteredOnConnect = false;
         mIsRegisteredOnDisconnect = false;
-    }
-
-    private boolean getDisplayInfo(Display display) {
-        mDisplayId = String.valueOf(display.getDisplayId());
-        mDisplayName = display.getName();
-        if (mDisplayId.equals(String.valueOf(display.DEFAULT_DISPLAY))) {
-            mIsPrimary = true;
-            mIsInternal = true;
-        } else {
-            mIsPrimary = false;
-            mIsInternal = false;
-        }
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        display.getRealMetrics(displayMetrics);
-        mDpiX = (int) displayMetrics.xdpi;
-        mDpiY = (int) displayMetrics.ydpi;
-        Point outSize = new Point();
-        display.getRealSize(outSize);
-        mWidth = outSize.x;
-        mHeight = outSize.y;
-        display.getSize(outSize);
-        mAvailableWidth = outSize.x;
-        mAvailableHeight = outSize.y;
-        return true;
     }
 
     private JSONObject setErrorMessage(String error) {
